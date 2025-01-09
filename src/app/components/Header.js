3cdbc71e44b35c5e8b10d593e8cd8d4a9500dd35
Shivam-Singh0 <ssshivamsingh151@gmail.com>
2025-01-10 02:00:41 +0530

    Header

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -23,16 +23,16 @@ export function Header() {
 
     const navList = (
         <ul className="mt-2 mb-4 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
-            <Typography
-                as="li"
+            <Link
+               href={"/upload"}
                 variant="small"
                 color="white"
                 className="p-1 font-normal hover:text-gray-300 transition-colors duration-200"
             >
-                <Link href="/upload" className="flex items-center">
+             
                     Upload
-                </Link>
-            </Typography>
+               
+            </Link>
            
         </ul>
     );
@@ -41,14 +41,12 @@ export function Header() {
         <div className="w-full overflow-x-hidden">
             <Navbar className="sticky top-0 z-10 h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4 bg-black shadow-md border-none"> {/* border-none added */}
                 <div className="flex items-center justify-between text-white">
-                    <Typography
-                        as="a"
-                        className="mr-4 cursor-pointer py-1.5 font-medium"
-                    >
+                   
+                    
                         <Link href="/" className="flex items-center">
-                    Upload
+                    GEO DATA
                 </Link>
-                    </Typography>
+                  
                     <div className="flex items-center gap-4">
                         <div className="mr-4 hidden lg:block">{navList}</div>
                         <div className="flex items-center gap-x-1">
